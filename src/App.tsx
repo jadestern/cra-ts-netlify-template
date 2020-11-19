5eefa5aafa8d920ba2bcf31d1400a6b6619007e9
고규훈 <kh.ko@airpremia.com>
2020-11-19 17:50:35 +0900

    ADD netlify function

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+  const load = async () => {
+    const { data } = await Axios.get('/.netlify/functions/hello');
+    console.log(data);
+  };
+
+  useEffect(() => {
+    load();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
